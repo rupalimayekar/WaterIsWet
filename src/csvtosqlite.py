43061235c258bb3892ce33369ec1702b1aa994ea
lukepharr <30753703+lukepharr@users.noreply.github.com>
2018-05-22 11:09:34 -0500

    yes

--- src/csvtosqlite.py
+++ src/csvtosqlite.py
@@ -21,14 +21,30 @@ class Data(Base):
     __table_args__ = {'sqlite_autoincrement': True}
     #tell SQLAlchemy the name of column and its attributes:
     id = Column(Integer, primary_key=True, nullable=False) 
-    Area_id = Column(VARCHAR)
-    Year = Column(Integer)
-    _4114 = Column(Float)
+    country = Column(VARCHAR)
+    area_id = Column(Integer)
+    year = Column(Integer)
+    area = Column(Float)
+    perc_cultivated = Column(Float)
+	total_pop = Column(Float)
+	rural_pop = Column(Float)
+	urban_pop = Column(Float)
+	pop_density = Column(Float)
+	gdp_per_cap = Column(Float)
+	hdi = Column(Float)
+	gii = Column(Float)
+	perc_undernourish = Column(Float)
+	nri = Column(Float)
+	dependency_ratio = Column(Float)
+	renew_water_per_cap = Column(Float)
+	water_stress = Column(Float)
+	flood = Column(Float)
+	perc_safe_water = Column(Float)
 
 
-engine = create_engine('sqlite:///data.sqlite')
+engine = create_engine('sqlite:///../data/data.sqlite')
 Base.metadata.create_all(engine)
-file_name = 'data/test.csv'
+file_name = '../data/test.csv'
 df = pd.read_csv(file_name)
 df.to_sql(con=engine, index_label='id', name=Data.__tablename__, if_exists='replace')
 
