7c3a16bdabac56eefcc377cf756c9101c0cfc9c7
acanois <david.richter42@gmail.com>
2018-05-30 17:07:32 -0500

    add hdi and gii plots to /hdi and /gii routes

--- src/app/static/js/gii.js
+++ src/app/static/js/gii.js
@@ -16,6 +16,11 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
   giiDataPoints2010 = [];
   giiDataPoints2015 = [];
 
+  // arrays of data points for the three years for the bubble plot
+  giiBubbleDataPoints2000 = [];
+  giiBubbleDataPoints2010 = [];
+  giiBubbleDataPoints2015 = [];
+
   // Loop through the response and create the js objects for the plots for the 2000 year bucket
   for (var i=0; i<response.year2000.country.length; i++) {
 
@@ -26,6 +31,14 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: response.year2000.country[i]
     }
     giiDataPoints2000.push(giiDataPoint);
+
+    // Bubble plot data
+    giiBubbleDataPoint = {
+      x: response.year2000.gdp[i], 
+      y: response.year2000.gii[i],
+      name: response.year2000.country[i]
+    }
+    giiBubbleDataPoints2000.push(giiBubbleDataPoint);
   };
 
   // Loop through the response and create the js objects for the plots for the 2010 year bucket
@@ -38,6 +51,14 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: response.year2010.country[i]
     }
     giiDataPoints2010.push(giiDataPoint);
+
+    // Bubble plot data
+    giiBubbleDataPoint = {
+      x: response.year2010.gdp[i], 
+      y: response.year2010.gii[i],
+      name: response.year2010.country[i]
+    }
+    giiBubbleDataPoints2010.push(giiBubbleDataPoint);
   };
 
   // Loop through the response and create the js objects for the plots for the 2015 year bucket
@@ -50,6 +71,14 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: response.year2015.country[i]
     }
     giiDataPoints2015.push(giiDataPoint);
+
+    // Bubble plot data
+    giiBubbleDataPoint = {
+      x: response.year2015.gdp[i], 
+      y: response.year2015.gii[i],
+      name: response.year2015.country[i]
+    }
+    giiBubbleDataPoints2015.push(giiBubbleDataPoint);
   };
 
   // Create the Data Series object for the hdi plot
@@ -68,6 +97,25 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: "2013-2017",
       color: 'rgba(20, 175, 160, 1)',
       data: giiDataPoints2015
+    }
+  ];
+
+  // Create the data series object for the bubble plot
+  giiBubbleSeriesData = [
+    {
+      name: "1998-2002",
+      color: 'rgba(223, 83, 83, 1)',
+      data: giiBubbleDataPoints2000
+    },
+    {
+      name: "2008-2012",
+      color: 'rgba(119, 152, 191, 1)',
+      data: giiBubbleDataPoints2010
+    },
+    {
+      name: "2013-2017",
+      color: 'rgba(20, 175, 160, 1)',
+      data: giiBubbleDataPoints2015
     }
   ];
 
@@ -134,5 +182,77 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
 
     series: giiSeriesData
 
+  });
+
+   // Scatter plot comparing GDP and GII
+  Highcharts.chart('gdp-gii-container', {
+
+    chart: {
+      type: 'scatter',
+      plotBorderWidth: 1,
+      zoomType: 'xy'
+    },
+  
+    legend: {
+      enabled: false
+    },
+  
+    title: {
+      text: 'GDP VS GII'
+    },
+  
+    subtitle: {
+      text: 'Source: Aquastat'
+    },
+  
+    xAxis: {
+      gridLineWidth: 1,
+      title: {
+        text: 'Gross Domestic Product (GDP) per Capita'
+      },
+      labels: {
+        format: '{value}'
+      },
+    },
+  
+    yAxis: {
+      startOnTick: false,
+      endOnTick: false,
+      title: {
+        text: 'Gender Inequality Index (GII)'
+      },
+      labels: {
+        format: '{value}'
+      },
+      maxPadding: 0.2
+    },
+  
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br>',
+      pointFormat: '{point.name}: {point.x}, {point.y}'
+    },
+
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'bottom',
+      x: -10,
+      y: -60,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+  
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: false,
+          format: '{point.name}'
+        }
+      }
+    },
+  
+    series: giiBubbleSeriesData
+  
   });
 });
--- src/app/static/js/hdi.js
+++ src/app/static/js/hdi.js
@@ -22,6 +22,11 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
   bubbleDataPoints2010 = [];
   bubbleDataPoints2015 = [];
 
+  // arrays of data points for the three years for the bubble plot
+  hdiBubbleDataPoints2000 = [];
+  hdiBubbleDataPoints2010 = [];
+  hdiBubbleDataPoints2015 = [];
+
 // Loop through the response and create the js objects for the plots for the 2000 year bucket
   for (var i=0; i<response.year2000.country.length; i++) {
     // hdi plot data
@@ -39,6 +44,13 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: response.year2000.country[i]
     }
     bubbleDataPoints2000.push(bubbleDataPoint);
+
+    hdiBubbleDataPoint = {
+      x: response.year2000.gdp[i], 
+      y: response.year2000.hdi[i],
+      name: response.year2000.country[i]
+    }
+    hdiBubbleDataPoints2000.push(hdiBubbleDataPoint)
   };
 
   // Loop through the response and create the js objects for the plots for the 2010 year bucket
@@ -59,6 +71,12 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
     }
     bubbleDataPoints2010.push(bubbleDataPoint);
       
+    hdiBubbleDataPoint = {
+      x: response.year2010.gdp[i], 
+      y: response.year2010.hdi[i],
+      name: response.year2010.country[i]
+    }
+    hdiBubbleDataPoints2010.push(hdiBubbleDataPoint)
    };
 
   // Loop through the response and create the js objects for the plots for the 2015 year bucket
@@ -79,6 +97,13 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
       name: response.year2015.country[i]
     }
     bubbleDataPoints2015.push(bubbleDataPoint);
+
+    hdiBubbleDataPoint = {
+      x: response.year2015.gdp[i], 
+      y: response.year2015.hdi[i],
+      name: response.year2015.country[i]
+    }
+    hdiBubbleDataPoints2015.push(hdiBubbleDataPoint)
   };
 
   // Create the Data Series object for the hdi plot
@@ -116,6 +141,25 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
         name: "2013-2017",
         color: 'rgba(20, 175, 160, 1)',
         data: bubbleDataPoints2015
+    }
+  ];
+
+  // Create the data series object for the bubble plot
+  hdiBubbleSeriesData = [
+    {
+      name: "1998-2002",
+      color: 'rgba(223, 83, 83, 1)',
+      data: hdiBubbleDataPoints2000
+    },
+    {
+      name: "2008-2012",
+      color: 'rgba(119, 152, 191, 1)',
+      data: hdiBubbleDataPoints2010
+    },
+    {
+      name: "2013-2017",
+      color: 'rgba(20, 175, 160, 1)',
+      data: hdiBubbleDataPoints2015
     }
   ];
 
@@ -256,5 +300,76 @@ d3.json('/hdi-gdp-gii-data', function(error, response) {
   
     series: bubbleSeriesData
   
-  });  
+  }); 
+
+  // Bubble plot comparing HDI, GII and GDP
+  Highcharts.chart('gdp-hdi-container', {
+    chart: {
+      type: 'scatter',
+      plotBorderWidth: 1,
+      zoomType: 'xy'
+    },
+  
+    legend: {
+      enabled: false
+    },
+  
+    title: {
+      text: 'GDP VS HDI'
+    },
+  
+    subtitle: {
+      text: 'Source: Aquastat'
+    },
+  
+    xAxis: {
+      gridLineWidth: 1,
+      title: {
+        text: 'Gross Domestic Product (GDP) per Capita'
+      },
+      labels: {
+        format: '{value}'
+      },
+    },
+  
+    yAxis: {
+      startOnTick: false,
+      endOnTick: false,
+      title: {
+        text: 'Human Development Index (HDI)'
+      },
+      labels: {
+        format: '{value}'
+      },
+      maxPadding: 0.2
+    },
+  
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br>',
+      pointFormat: '{point.name}: {point.x}, {point.y}'
+    },
+
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'bottom',
+      x: -10,
+      y: -60,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+      borderWidth: 1
+    },
+  
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: false,
+          format: '{point.name}'
+        }
+      }
+    },
+  
+    series: hdiBubbleSeriesData
+  
+  }); 
 });
